e1d8c3a53989e1af4e785971892bef6de537492c
FarhatA203040060 <203040060@mail.unpas.ac.id>
2022-09-30 15:30:36 +0700

    Mengganti Warna dan menambah style.css

--- pw/index.php
+++ pw/index.php
@@ -9,10 +9,11 @@ $buku = query("SELECT * FROM buku");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Daftar Buku</title>
 </head>
 
-<body class = "bg-success">
+<body>
 <div class="container ">
   <div class="card mt-5 bg-white text-light">
     <div class="card-body text-light">
