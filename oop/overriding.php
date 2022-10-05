df605c1e0dd18c519af16716de863627d201c6e2
FarhatA22 <farhatabd21@gmail.com>
2022-10-05 21:53:27 +0700

    typo

--- oop/overriding.php
+++ oop/overriding.php
@@ -76,7 +76,7 @@ class CetakInfoProduk {
 
 
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk1 = new Komik("Naruta", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
 $produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
  
 
