71e286942ac20bd6bca0765f727584bab8ee99e0
FarhatA22 <farhatabd21@gmail.com>
2022-10-13 22:48:10 +0700

    Menyelesaikan Video ke-5 dan ke-6

---
+++ mvc/app/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+<div class="jumbotron mt-5">
+        <h1 class="display-4">Selamat Datang di Website Farhat</h1>
+        <p class="lead">Nama saya Farhat 203040060</p>
+        <hr class="my-4">
+        <p>Saya adalah Mahasiswa di Universitas Pasundan yang beralamat di jl.setiabudi</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">learn more</a>
+    </div>
+</div>
