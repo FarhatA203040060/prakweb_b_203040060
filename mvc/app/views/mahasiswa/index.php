584d5c9a7bd19a7eb7cb2641d585f58cecdc2909
FarhatA22 <farhatabd21@gmail.com>
2022-10-14 13:20:07 +0700

    Memperbaiki Tampilan

--- mvc/app/views/mahasiswa/index.php
+++ mvc/app/views/mahasiswa/index.php
@@ -7,7 +7,7 @@
         <?php foreach ($data['mhs'] as $mhs) : ?>
           <li class="list-group-item d-flex justify-content-between align-items-center">
             <?= $mhs['nama']; ?>
-            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary">datail</a>
+            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary">Detail</a>
           </li>
         <?php endforeach; ?>
       </ul>
